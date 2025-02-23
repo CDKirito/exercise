4c99e70381373402a7bd120f3a97ae9614f54d6c
CDKirito <2586859677@qq.com>
2025-02-23 21:09:39 +0800

    second save

--- rw_visual.py
+++ rw_visual.py
@@ -14,8 +14,8 @@ ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecol
 ax.set_aspect('equal')
 
 # 突出起点和终点
-# ax.scatter(0, 0, c='green', edgecolors='none', s=100)
-# ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
 
 # 隐藏坐标轴
 ax.get_xaxis().set_visible(False)
